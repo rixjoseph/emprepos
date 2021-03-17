48b6605b03c2d66d88067e3deb78dc81ff78c6f4
rixjoseph <mca517@rajagiri.edu>
2021-03-17 02:52:40 -0700

    EMP1

--- resources/views/empsearch.blade.php
+++ resources/views/empsearch.blade.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+@extends("theme")
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SEARCH</title>
-</head>
-<nav class="navbar navbar-dark bg-success">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">ADD</a>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-<body>
+
+@section("content")
+
 <div class="container">
     <div class="row">
         <div class="col col-12">
@@ -34,6 +19,6 @@
         </div>
     </div>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>  
-</body>
-</html>
+
+
+@endsection
